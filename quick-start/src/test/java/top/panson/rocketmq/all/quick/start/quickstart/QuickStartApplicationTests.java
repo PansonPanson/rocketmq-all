d2826d95a5c3807e7d20a999f03f500f246acf38
Panson <PansonPanson@163.com>
2022-11-23 00:26:01 +0800

    add pom.xml

---
+++ quick-start/src/test/java/top/panson/rocketmq/all/quick/start/quickstart/QuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package top.panson.rocketmq.all.quick.start.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
