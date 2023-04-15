566ca34b5834f7eb08982efa5f3b978c270beb92
Panson <PansonPanson@163.com>
2023-04-15 17:40:21 +0800

    auto commit

---
+++ draft/draft.md
@@ -0,0 +1,5 @@
++ 今日头条在消息服务平台和容灾体系建设方面的实践与思考: https://www.infoq.cn/article/xpfz4j978rgprxergjok
++ 网易云音乐的消息队列改造之路：https://www.infoq.cn/article/2_d683szyss0pjmdkxg2
++ 我与消息队列的八年情缘：https://www.modb.pro/db/214546
++ 哈啰在分布式消息治理和微服务治理中的实践：https://mp.weixin.qq.com/s/N-vd6he4nsZp-G3Plc4m6A
+
--- quick-start/src/main/java/top/panson/rocketmq/Application.java
+++ quick-start/src/main/java/top/panson/rocketmq/Application.java
@@ -3,11 +3,22 @@ package top.panson.rocketmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        List<Long> list = new ArrayList<>();
+        list.add(1L);
+        list.add(1L);
+        list.add(2L);
+        list.add(2L);
+        System.out.println(list.stream().distinct().collect(Collectors.toList()));
+
     }
 
 }
